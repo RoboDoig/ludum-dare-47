a247a7e419977bd1406e9fd6fefc4867b02f563d
Andrew Erskine <erskine94@gmail.com>
2020-10-03 12:25:42 -0700

    Initial commit

---
+++ Assets/Character.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rig;
+    [SerializeField]
+    private Transform groundCheck;
+    private float groundedRadius = 0.1f;
+    private bool isGrounded;
+    public float speed = 6f;
+    public float vSpeedMod = 1f;
+    public float jumpForce = 6f;
+
+    private bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        // Check if character is grounded
+        isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                isGrounded = true;
+        }
+
+        // Update the animator
+        animator.SetFloat("hSpeedMag", Mathf.Abs(rig.velocity.x));
+        animator.SetFloat("vSpeed", rig.velocity.y);
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    public void Move(float horizontalInput, float verticalInput, bool jump) {
+        rig.velocity = new Vector2(horizontalInput * speed, rig.velocity.y + (verticalInput * vSpeedMod));
+
+        if (horizontalInput > 0 && !facingRight) {
+            Flip();
+        } else if (horizontalInput < 0 && facingRight) {
+            Flip();
+        }
+
+        if (jump && isGrounded) {
+            rig.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Character character;
+
+    private bool jump = false;
+
+    void Start() {
+        character = GetComponent<Character>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v > 0) {
+            v = 0;
+        }
+
+        character.Move(h, v, jump);
+        jump = false;
+    }
+}
